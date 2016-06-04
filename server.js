89d74f3ece6489480d0406cea5b14b1422024e8a
nvm <ncc0706@gmail.com>
2016-06-05 01:48:14 +0800

    add url.parse

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 var http = require('http');
+var url  = require('url');
+
+function start(){
+ function onRequest(request, response){
+  var pathname = url.parse(request.url).pathname;
+  console.log("Request for " + pathname	+ " received.");
+
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('<h3>Hello World</h3>');
+  response.write('<h4>nodemon server.js</h4>');
+  response.end();
+ }
+ 
+ http.createServer(onRequest).listen(8080);
+ console.log('Server has started');
+}
+
 
 /*
 http.createServer(function(request, response){
@@ -39,20 +56,6 @@ http.createServer(onRequest).listen(8080);
 console.log('Server has started');
 */
 
-
-
-function start(){
- function onRequest(request, response){
-  console.log("Request	received.");
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write('<h3>Hello World</h3>');
-  response.write('<h4>nodemon server.js</h4>');
-  response.end();
- }
- 
- http.createServer(onRequest).listen(8080);
- console.log('Server has started');
-}
 
 exports.start = start;
 
