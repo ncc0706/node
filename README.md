377cc15f7a1f54cbe1bc300a0e64d944793e7d67
nvm <ncc0706@gmail.com>
2016-06-05 01:27:31 +0800

    http createServer 传递匿名函数

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ helloworld.js helloworld
 
 $node filename 都会显示文件里面的内容,说明node不是根据文件的后缀来执行文件的.
 
+参数传递来一个匿名函数
+http.createServer();
 
+**基于事件驱动的回调**
 
 
---
+++ server.js
@@ -0,0 +1,29 @@
+var http = require('http');
+
+/*
+http.createServer(function(request, response){
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('Hello World');
+  response.end();
+}).listen(8080);
+*/
+
+/*
+http.createServer(function(request, response){
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('<h2>Hello World</h2>');
+  response.end();
+}).listen(8080);
+*/
+
+function onRequest(request, response){
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('<h3>Hello World</h3>');
+  response.write('<h4>nodemon server.js</h4>');
+  response.end();
+}
+
+http.createServer(onRequest).listen(8080);
+
+
+
